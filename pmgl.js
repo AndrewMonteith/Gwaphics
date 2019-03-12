b6a35ac0fde7f3b7f9e61b035040c48b0b5f00ed
AndrewMonteith <andrewmonteith1999@gmail.com>
2019-03-12 14:50:34 +0000

    mipmapping is now good

--- pmgl.js
+++ pmgl.js
@@ -598,7 +598,8 @@ class Scene {
     // Bind the texture object to the target
     gl.bindTexture(gl.TEXTURE_2D, glTexture);
     
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
     
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
